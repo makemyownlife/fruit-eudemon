95719e114fa356118208ec87071c16395eb31ff1
zhangyong <zhangyong7120180@163.com>
2021-08-05 15:36:48 +0800

    [增加] fruit 水果捕捉

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"fruit-eudemon/meican"
+	"github.com/sirupsen/logrus"
+	"time"
+)
 
 func main() {
-	logrus.Info("hello , 新鲜水果 开始抓取程序")
+	var ch chan int
+	logrus.Info("hello, 新鲜水果开始抓取程序")
+	//定时任务
+	ticker := time.NewTicker(time.Second * 5)
+	go func() {
+		for range ticker.C {
+			meican.DoTask()
+		}
+		ch <- 1
+	}()
+	<-ch
 }
---
+++ meican/HttpFruit.go
@@ -0,0 +1,5 @@
+package meican
+
+func DoTask() {
+
+}
